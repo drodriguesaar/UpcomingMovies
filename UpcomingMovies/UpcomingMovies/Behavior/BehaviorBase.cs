ad455f5afc49467a30f8f01f94dd177405793545
Daniel Rodrigues de Almeida Araújo <drodriguesaar@gmail.com>
2019-03-20 14:39:32 -0300

    Behavior refactor classes repalced by a general event to command behavior class.

---
+++ UpcomingMovies/UpcomingMovies/Behavior/BehaviorBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UpcomingMovies.Behavior
+{
+    public class BehaviorBase<TView> : Behavior<TView> where TView : BindableObject
+    {
+        public TView AssociatedObject { get; private set; }
+        protected override void OnAttachedTo(TView bindable)
+        {
+            base.OnAttachedTo(bindable);
+            AssociatedObject = bindable;
+            if (bindable.BindingContext != null)
+            {
+                BindingContext = bindable.BindingContext;
+            }
+            bindable.BindingContextChanged += OnBindingContextChanged;
+        }
+        protected override void OnDetachingFrom(TView bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+            AssociatedObject = null;
+        }
+        protected void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+    }
+}
